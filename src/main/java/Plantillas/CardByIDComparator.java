6c12b72f481e0ff633f81ffb819f446ae4f86063
LeoOlivaresD <cspraword@gmail.com>
2023-03-30 22:49:08 -0300

    Refactorizacion

--- src/main/java/Plantillas/CardByIDComparator.java
+++ src/main/java/Plantillas/CardByIDComparator.java
@@ -1,4 +1,4 @@
-package Plantillas.yugioh;
+package Plantillas;
 
 import java.util.Comparator;
 
@@ -6,13 +6,11 @@ public class CardByIDComparator extends CartasYuGiOh implements Comparator<Carta
 
     @Override
     public int compare(CartasYuGiOh carta1, CartasYuGiOh carta2) {
-        int respuesta =0;
-        if (carta1.getIdCard() > carta2.getIdCard()){
+        int respuesta = 0;
+        if (carta1.getIdCard() > carta2.getIdCard()) {
             respuesta = 1;
         } else if (carta1.getIdCard() < carta2.getIdCard()) {
-            respuesta =-1;
-        }else{
-            respuesta = 0;
+            respuesta = -1;
         }
         return respuesta;
     }
